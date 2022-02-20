a484ddf0afb2fec56a81470dfc5600d14e3832b9
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-02-20 15:06:05 +0900

    Loadingを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,33 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import '../styles/globals.css'
+import { useEffect } from 'react'
+import type { AppProps } from 'next/app'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import NProgress from 'nprogress'
+import '../public/nprogress.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`)
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
   return (
     <>
       <Header />
