b2f2e508ab16f36bc264244cf6a62772b3c48e7b
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-02-20 15:46:47 +0900

    useRouterを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
