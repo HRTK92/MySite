9b3e355bc7cf6596b21d5fbad48dbc942b46a671
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-07-26 04:12:53 +0000

    /MySiteを追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,28 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/MySite/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/MySite/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/MySite/favicon-16x16.png"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
