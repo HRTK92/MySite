973403749798b9d05553a9da3000efa1e8a53ed0
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-07-26 04:02:27 +0000

    urlPrefixを変更

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const urlPrefix = process.env.GITHUB_ACTIONS ? "/mysite" : ""
+const urlPrefix = process.env.GITHUB_ACTIONS ? "/MySite" : ""
 
 module.exports = {
   reactStrictMode: true,
