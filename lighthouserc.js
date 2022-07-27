56dd490c9d7c70261fe7ad8c0aad43d6e17aa0d8
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-07-27 23:36:20 +0000

    評価するURLを追加

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,12 @@ module.exports = {
     collect: {
       numberOfRuns: 1,
       startServerCommand: 'yarn start',
-      url: ['http://localhost:3000/'],
+      url: [
+        'http://localhost:3000/',
+        'http://localhost:3000/articles',
+        'http://localhost:3000/github/links',
+
+    ],
     },
     upload: {
       target: 'temporary-public-storage',
