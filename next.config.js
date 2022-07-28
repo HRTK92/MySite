d5181313b037f69f37b4713c1716764c982ac483
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-07-28 18:26:09 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const urlPrefix = process.env.GITHUB_ACTIONS ? process.env.urlPrefix : ""
-
 module.exports = {
-  reactStrictMode: true,
-  assetPrefix: urlPrefix,
-  basePath: urlPrefix,
+  
 };
