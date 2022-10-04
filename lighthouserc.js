198592206e27cf991bad9522565d7633674e617b
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-10-04 10:25:32 +0000

    lighthouseの設定を変更

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,8 +6,6 @@ module.exports = {
       url: [
         'http://localhost:3000/',
         'http://localhost:3000/articles',
-        'http://localhost:3000/github/links',
-
     ],
     },
     upload: {
