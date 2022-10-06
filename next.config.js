3f57f716efacbc937ea3e65a627685eda90a0831
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-10-06 08:40:56 +0000

    開発時のbaseurlを指定

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 
+require('dotenv').config()
+
 module.exports = {
-  
+  basePath: process.env.GITHUB_PAGES ? '' : '/MySite',
+  //assetPrefix: '/MySite',
 };
