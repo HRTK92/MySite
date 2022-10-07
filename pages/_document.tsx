2489d80b6cd7cc3b5d5069d0192e89dad415dc34
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-10-07 00:00:24 +0000

    wow.jsを追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang='ja'>
         <Head>
-          <link rel="canonical" href="https://hrtk92.github.io/MySite/"/>
+          <link rel='canonical' href='https://hrtk92.github.io/MySite/' />
           <link rel='apple-touch-icon' sizes='180x180' href='/MySite/apple-touch-icon.png' />
           <link rel='icon' type='image/png' sizes='32x32' href='/MySite/favicon-32x32.png' />
           <link rel='icon' type='image/png' sizes='16x16' href='/MySite/favicon-16x16.png' />
@@ -17,8 +17,10 @@ class MyDocument extends Document {
           <meta property='og:image' content='https://hrtk92.github.io/MySite/og_image.jpeg' />
           <meta property='og:locale' content='ja_JP' />
           <meta property='og:type' content='website' />
-
           <meta name='twitter:card' content='summary_large_image' />
+
+          <script src='https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js'></script>
+          <script>new WOW().init();</script>
         </Head>
         <body>
           <Main />
