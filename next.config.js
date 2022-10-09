8d540c089a8e5e723cdd76c9064f2436658bd005
はらたく <70054655+HRTK92@users.noreply.github.com>
2022-10-09 02:40:14 +0000

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@
 require('dotenv').config()
 
 module.exports = {
-  basePath: process.env.GITHUB_PAGES ? '' : '/MySite',
+  //basePath: process.env.GITHUB_PAGES ? '' : '/MySite',
   //assetPrefix: '/MySite',
 }
