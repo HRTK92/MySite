4a7a070f7b2543d94d7923b417de66a39dfb5510
はらたく <70054655+HRTK92@users.noreply.github.com>
2023-05-23 11:38:38 +0000

    フォーマット

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 import useSWR from 'swr'
 import { ArticleList } from '../types/article'
 import { useEffect, useState } from 'react'
-import { ArrowDown, Heart } from "@phosphor-icons/react";
+import { ArrowDown, Heart } from '@phosphor-icons/react'
 
 export default function Home() {
   const { data: zennFeed } = useSWR('/api/zennFeed', (url: string) =>
@@ -74,13 +74,15 @@ export default function Home() {
             <div className='flex animate-tracking-in-expand-fwd-bottom flex-row justify-center'>
               <a
                 href='https://github.com/HRTK92'
-                className='duration-5000 mx-1 rounded-xl bg-gray-200 p-2 shadow-md transition hover:scale-125 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-blue-500'>
+                className='duration-5000 mx-1 rounded-xl bg-gray-200 p-2 shadow-md transition hover:scale-125 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-blue-500'
+              >
                 <img src='/social_icons/github.svg' alt='GitHub' className='h-7 w-7' />
               </a>
 
               <a
                 href='#zenn'
-                className='duration-5000 mx-1 rounded-xl bg-gray-200 p-2 shadow-md transition hover:scale-125 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-blue-500'>
+                className='duration-5000 mx-1 rounded-xl bg-gray-200 p-2 shadow-md transition hover:scale-125 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-blue-500'
+              >
                 <img src='/social_icons/zenn.png' alt='Zenn' className='h-7 w-7 rounded-full' />
               </a>
 
@@ -88,28 +90,29 @@ export default function Home() {
                 href='#'
                 className='duration-5000 mx-1 rounded-xl bg-gray-200 p-2
               shadow-md transition hover:scale-125 hover:bg-gradient-to-r
-              hover:from-cyan-400 hover:to-blue-500 '>
+              hover:from-cyan-400 hover:to-blue-500 '
+              >
                 <img src='/social_icons/twitter.svg' alt='Twitter' className='h-7 w-7' />
               </a>
 
               <a
                 href='https://discord.com/users/618332297275375636'
-                className='duration-5000 mx-1 rounded-xl bg-gray-200 p-2 shadow-md transition hover:scale-125 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-blue-500'>
+                className='duration-5000 mx-1 rounded-xl bg-gray-200 p-2 shadow-md transition hover:scale-125 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-blue-500'
+              >
                 <img src='/social_icons/discord.svg' alt='Discord' className='h-7 w-7' />
               </a>
             </div>
 
             <a className='mt-12 flex justify-center' href='#skills'>
-              <ArrowDown
-                className='h-7 w-7 animate-bounce-slow fill-black text-gray-500 transition hover:scale-125 hover:text-gray-700'
-              />
+              <ArrowDown className='h-7 w-7 animate-bounce-slow fill-black text-gray-500 transition hover:scale-125 hover:text-gray-700' />
             </a>
           </div>
         </div>
 
         <div
           className='flex h-screen w-screen snap-start flex-col rounded-t-2xl bg-gray-100 p-2 text-black'
-          id='skills'>
+          id='skills'
+        >
           <div className='flex flex-col p-2'>
             <p className='pb-5 text-4xl font-bold text-gray-700'>
               Skills <span className='text-sm text-gray-500'>スキル</span>
@@ -189,7 +192,8 @@ export default function Home() {
               className='flex flex-row rounded-md border bg-gray-800 p-2'
               onClick={() => {
                 router.push('https://github.com/HRTK92/next-boards')
-              }}>
+              }}
+            >
               <img src='/social_icons/github.svg' alt='GitHub' className='h-10 w-10 fill-black' />
               <div>
                 <p className='px-1 font-bold text-white'>HRTK92 / next-boards</p>
@@ -202,7 +206,8 @@ export default function Home() {
               className='cursor-pointer p-3 text-end text-gray-700 transition duration-500 hover:scale-105 hover:text-gray-900'
               onClick={() => {
                 router.push('https://github.com/HRTK92?tab=repositories&q=&type=&language=&sort=stargazers')
-              }}>
+              }}
+            >
               and more...
             </div>
           </div>
@@ -218,7 +223,8 @@ export default function Home() {
                 className='flex flex-row rounded-md p-2 shadow-md transition duration-500 hover:scale-105 hover:shadow-lg'
                 onClick={() => {
                   router.push('https://zenn.dev/hrtk92')
-                }}>
+                }}
+              >
                 <img
                   src={zennUser?.user.avatar_url}
                   alt='HRTK92'
@@ -242,7 +248,8 @@ export default function Home() {
                         setTimeout(() => {
                           router.push(item.link)
                         }, 500)
-                      }}>
+                      }}
+                    >
                       <div className='flex-none  rounded-xl bg-gray-200 p-2 shadow-md transition duration-500 hover:scale-105 hover:bg-blue-400'>
                         <img src={item.enclosure.link} alt={item.title} className='rounded-lg' />
                       </div>
